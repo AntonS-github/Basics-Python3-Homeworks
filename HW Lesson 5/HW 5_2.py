639905b2a773b76c36ce5eb066adedd99bcf2e84
AntonS <surkov.ved@gmail.com>
2022-04-21 22:09:05 +0300

    Done Lesson 5 HW

---
+++ HW Lesson 5/HW 5_2.py
@@ -0,0 +1,12 @@
+n = 15
+odd_num = (num for num in range(1, n+2, 2))
+print(next(odd_num))
+print(next(odd_num))
+print(next(odd_num))
+print(next(odd_num))
+print(next(odd_num))
+print(next(odd_num))
+print(next(odd_num))
+print(next(odd_num))
+print(next(odd_num))
+print(next(odd_num))
